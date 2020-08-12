70ada59dd2378031df5e375332261371f675ea34
Charlie Jo <charliejames1652@gmail.com>
2020-08-12 17:15:20 -0400

    env added

--- db/migrations/20200812446-create-user-table.sql
+++ db/migrations/20200812446-create-user-table.sql
@@ -1,5 +1,6 @@
-CREATE TABLE IF NOT EXISTS user_table 
+CREATE TABLE IF NOT EXISTS user_table (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     status BOOLEAN NOT NULL DEFAULT false,
-    email VARCHAR(150) NOT NULL,
+    email VARCHAR(150) NOT NULL
+);
