db673511145b151212903c4c4008bc32437b57a2
Charlie Jo <charliejames1652@gmail.com>
2020-08-12 17:01:40 -0400

    syntax errors

--- db/migrations/20200812446-create-user-table.sql
+++ db/migrations/20200812446-create-user-table.sql
@@ -1,7 +1,5 @@
-CREATE TABLE IF NOT EXISTS user_table (
+CREATE TABLE IF NOT EXISTS user_table 
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     status BOOLEAN NOT NULL DEFAULT false,
     email VARCHAR(150) NOT NULL,
-
-);
