6fcc29a2f1362131070355aa682bc1216e3229d3
Charlie Jo <charliejames1652@gmail.com>
2020-08-14 15:59:11 -0400

    working on models

--- models/water.js
+++ models/water.js
@@ -5,6 +5,14 @@ class water {
         this.id = water.id || null;
         this.integer = water.integer;
     }
+
+    static getAll(){
+        return db.manyOrNone
+        ('SELECT * FROM water ORDER BY id ASC')
+        .then(water =>{
+            return water.map(water => new this(water));
+        })
+    }
 }
 
 class user {
@@ -13,5 +21,13 @@ class user {
         this.name = user.name;
         this.status = user.status || false;
         this.email = user.email
+    }
+
+    static getAll(){
+        return db.manyOrNone
+        ('SELECT * FROM user ORDER BY id ASC')
+        .then(user =>{
+            return user.map(user => new this(user));
+        })
     }
 }
