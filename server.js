0100d305642e2c11e20b0b81e586361342b2d10e
Charlie Jo <charliejames1652@gmail.com>
2020-08-15 10:41:49 -0400

    saving server.js updates

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const logger = require('morgan');
+const bodyParser = require('body-parser');
 
 const waterLogRouter = require('./routes/waterLogRouter');
 
 const app = express();
 
 app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
