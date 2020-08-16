acbcfc213cf17d8bf474bd1db805aeeb2c6c9803
Charlie Jo <charliejames1652@gmail.com>
2020-08-16 15:36:15 -0400

    updated user table to coexist with water table

--- db/migrations/20200812446-create-user-table.sql
+++ db/migrations/20200812446-create-user-table.sql
@@ -1,6 +1,9 @@
 CREATE TABLE IF NOT EXISTS user_table (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
+    name VARCHAR(255) UNIQUE NOT NULL,
     status BOOLEAN NOT NULL DEFAULT false,
-    email VARCHAR(150) NOT NULL
+    email VARCHAR(150) UNIQUE NOT NULL,
+    password_digest TEXT NOT NULL,
 );
+
+ALTER TABLE water_table ADD COLUMN user_id INTEGER REFERENCES user_table(id);
