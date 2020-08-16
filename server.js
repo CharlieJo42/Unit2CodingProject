d454e8122ce7afe885eef062d343afbcd9212928
Charlie Jo <charliejames1652@gmail.com>
2020-08-16 16:36:14 -0400

    fixed error with water-log-router vs waterLogRouter

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const passport = require('passport');
 
-const waterLogRouter = require('./routes/waterLogRouter');
+const waterLogRouter = require('./routes/water-log-router');
 
 const app = express();
 
